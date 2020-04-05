1bd716fd2e80935fe2f7c65d2fd669d4acd2b2a9
zhuoyi <m2zhuoyi@163.com>
2020-04-05 19:30:44 +0800

    some change for bind.js

--- containers/bind.js
+++ containers/bind.js
@@ -49,8 +49,9 @@ if (!Function.prototype.bind) {
     };
 }
 
-//MDN 实现
+// MDN 实现
 // Does not work with `new funcA.bind(thisArg, args)`
+/*
 if (!Function.prototype.bind) (function(){
     var slice = Array.prototype.slice;
     Function.prototype.bind = function() {
@@ -67,4 +68,41 @@ if (!Function.prototype.bind) (function(){
             return thatFunc.apply(thatArg, funcArgs);
         };
     };
-})();
+})();*/
+(function(){
+    var slice = Array.prototype.slice;
+    Function.prototype.bind = function() {
+        var thatFunc = this, thatArg = arguments[0];
+        var args = slice.call(arguments, 1);
+        // console.log('thatArg', thatArg);
+        if (typeof thatFunc !== 'function') {
+            // closest thing possible to the ECMAScript 5
+            // internal IsCallable function
+            throw new TypeError('Function.prototype.bind - ' +
+                'what is trying to be bound is not callable');
+        }
+        return function(){
+            console.log('arguments', arguments);
+            console.log('args', args);
+            var funcArgs = args.concat(slice.call(arguments))
+            console.log('funcArgs', funcArgs);
+            return thatFunc.apply(thatArg, funcArgs);
+        };
+    };
+})()
+
+var obj = {
+    name: '小张',
+    objAge: this.age,
+    myFun: function (fm, t) {
+        console.log(this.name + this.age, '来自' + fm + '去往' + t)
+    }
+}
+
+var db = {
+    name: 'Danika',
+    age: 99
+}
+
+obj.myFun.bind(db, ['北京', '上海'], '天津')();
+
