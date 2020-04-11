9326602f635faceea6394c4ea1dde8a62cbac0a7
zhuoyi <m2zhuoyi@163.com>
2020-04-11 17:13:10 +0800

    add two-way-binding

--- containers/rebuildBinaryTree.js
+++ containers/rebuildBinaryTree.js
@@ -4,7 +4,6 @@ function reConstructBinaryTree(pre, vin) {
     if (!pre || vin.length === 0) {
         return;
     };
-    
 
     var index = vin.indexOf(pre[0]);
     var left = vin.slice(0, index);
@@ -15,5 +14,4 @@ function reConstructBinaryTree(pre, vin) {
         left: reConstructBinaryTree(pre.slice(1, index + 1 ), left),
         right: reConstructBinaryTree(pre.slice(index + 1), right)
     }
-
 }
