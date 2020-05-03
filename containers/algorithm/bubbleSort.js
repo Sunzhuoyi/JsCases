d89a6e034e65f3d4833f3e3f19e08aa91bd17377
zhuoyi <m2zhuoyi@163.com>
2020-05-03 18:31:58 +0800

    add bubblesort

--- containers/algorithm/bubbleSort.js
+++ containers/algorithm/bubbleSort.js
@@ -1,9 +1,22 @@
 let arr = [4,5,6,3,2,1];
 
 function bubbleSort(arr) {
-    let l = arr.length;
-    if (l <= 1) return;
-    for () {
-
+    let n = arr.length;
+    if (n <= 1) return;
+    for (let i = 0; i < n; i++) {
+        let flag = false;
+        for (let j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let tem = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tem;
+                flag = true
+            }
+        }
+        if (!flag) break;
     }
 }
+
+bubbleSort(arr);
+
+console.log(20, arr)
