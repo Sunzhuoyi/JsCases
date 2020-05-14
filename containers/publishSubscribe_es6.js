c631c48a08699b0dd1317ab89c57e8ea091a7550
zhuoyi <m2zhuoyi@163.com>
2020-05-14 23:22:12 +0800

    add publish_subscribe_es6

--- containers/publishSubscribe_es6.js
+++ containers/publishSubscribe_es6.js
@@ -9,11 +9,43 @@ class PubSub {
     }
 
     publish() {
-        var event = Array.prototype.shift.call(arguments);
-
+        let event = Array.prototype.shift.call(arguments);
+        let handles = this.events[event];
+        let length = handles.length;
+        for (var i = 0; i < length; i++ ) {
+                handles[i].apply(this, arguments)
+        }
     }
 
-    remove() {
+    remove(event, fn) {
+        let handles = this.events[event];
+        if (!handles) return;
+        if (!fn) this.events[event] = [];
 
+        for (var i = 0; i < handles.length; i++) {
+            if (handles[i] === fn) {
+                handles.slice(i, 1)
+            }
+        }
     }
 }
+
+let pubsub = new PubSub();
+pubsub.subscribe('eventOne', function () {
+    console.log('eventone')
+});
+
+function test() {
+    console.log('eventOne otherone');
+}
+
+pubsub.subscribe('eventOne', test);
+
+pubsub.subscribe('eventTwo', function (data) {
+    console.log(data);
+});
+
+pubsub.remove('eventOne', test);
+
+pubsub.publish('eventOne');
+pubsub.publish('eventTwo', { message: 'hello' });
