d493c1d9d5b315072358321955515bedad8aa1ce
zhuoyi <m2zhuoyi@163.com>
2020-06-10 19:04:02 +0800

    find the last element that is less than or equal to the given value

--- containers/algorithm/binaryFind.js
+++ containers/algorithm/binaryFind.js
@@ -44,3 +44,25 @@ const arrFirst = [1, 4,4, 5, 6, 7, 8, 10, 11, 23, 42, 44,44,44, 54, 56, 77, 102]
 /*console.log(binaryFindFirst(arrFirst,4))
 console.log(binaryFindFirst(arrFirst,44))
 console.log(binaryFindFirst(arrFirst,41))*/
+
+// 查找最后一个小于等于给定值的元素
+const binaryFindLastSmall = (arr, target) => {
+    if (arr.length === 0) return -1;
+    let low = 0;
+    let high = arr.length - 1;
+    while (low <= high) {
+        const mid = Math.floor((low + high) / 2);
+        if (target < arr[mid]) {
+            high = mid - 1
+        } else {
+            if (target < arr[mid + 1] || mid === arr.length - 1) return mid;
+            low = mid + 1
+        }
+    };
+    return -1
+};
+
+const arrLastSmall = [1, 4,4,4, 5, 42, 44,44,44, 54, 56, 77, 102];
+console.log(binaryFindLastSmall(arrLastSmall,4))
+console.log(binaryFindLastSmall(arrLastSmall,44))
+console.log(binaryFindLastSmall(arrLastSmall,41))
